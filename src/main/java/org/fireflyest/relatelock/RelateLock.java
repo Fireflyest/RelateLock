c1f22164fa89c46bc875a07b4505a0fe5a26cf4f
Fireflyest <fireflyest@qq.com>
2024-06-10 21:38:11 +0800

    initial commit

---
+++ src/main/java/org/fireflyest/relatelock/RelateLock.java
@@ -0,0 +1,28 @@
+package org.fireflyest.relatelock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RelateLock extends JavaPlugin {
+    
+    public static RelateLock getPlugin() {
+        return getPlugin(RelateLock.class);
+    }
+
+    @Override
+    public void onEnable() {
+        // data service
+        
+        // gui service
+        
+        // listener
+        
+        // commands
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // close data service
+        
+    }
+}
