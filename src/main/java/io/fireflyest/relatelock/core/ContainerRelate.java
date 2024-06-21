549f8bb39a7f3fce5993c6a31b61da657d56c4da
Fireflyest <fireflyest@qq.com>
2024-06-22 00:31:16 +0800

    容器锁

--- src/main/java/io/fireflyest/relatelock/core/ContainerRelate.java
+++ src/main/java/io/fireflyest/relatelock/core/ContainerRelate.java
@@ -2,20 +2,33 @@ package io.fireflyest.relatelock.core;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Container;
 
+/**
+ * 容器锁
+ * @author Fireflyest
+ * @since 1.0
+ */
 public class ContainerRelate extends Relate {
 
     protected ContainerRelate(@Nullable Block signBlock, @Nonnull Block attachBlock) {
         super(signBlock, attachBlock);
-        //TODO Auto-generated constructor stub
     }
 
     @Override
     public Relate traceRelateBlocks() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'traceRelateBlocks'");
+        if (attachBlock.getState() instanceof Container) {
+            // 容器本身
+            relateBlocks.add(attachBlock);
+            // 容器下方方块
+            final Block downBlock = attachBlock.getRelative(BlockFace.DOWN);
+            if (!downBlock.isEmpty() && !downBlock.isLiquid() && !downBlock.isPassable()) {
+                relateBlocks.add(downBlock);
+            }
+        }
+        return this;
     }
     
 }
