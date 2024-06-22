0c2f74462bcaa1b83d49579ba6116b31968d1885
Fireflyest <fireflyest@qq.com>
2024-06-22 10:58:30 +0800

    门底方块保护

--- src/main/java/io/fireflyest/relatelock/core/BisectedRelate.java
+++ src/main/java/io/fireflyest/relatelock/core/BisectedRelate.java
@@ -22,18 +22,33 @@ public class BisectedRelate extends Relate {
         if (attachBlock instanceof Bisected bisected) {
             // 自己
             relateBlocks.add(attachBlock);
-            // 另一部分
-            final BlockFace face = switch (bisected.getHalf()) {
-                case TOP -> BlockFace.DOWN;
-                case BOTTOM -> BlockFace.UP;
-                default -> null;
-            };
+            // 另一部分，和基座
+            final BlockFace face;
+            final Block baseBlock;
+            switch (bisected.getHalf()) {
+                case TOP -> {
+                    face = BlockFace.DOWN;
+                    baseBlock = attachBlock.getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN);
+                }
+                case BOTTOM -> {
+                    face = BlockFace.UP;
+                    baseBlock = attachBlock.getRelative(BlockFace.DOWN);
+                }
+                default -> {
+                    face = null;
+                    baseBlock = null;
+                }
+            }
             if (face != null) {
                 final Block otherPart = attachBlock.getRelative(face);
                 // 某些情况下门只有一半
                 if (otherPart.getBlockData() instanceof Bisected) {
                     relateBlocks.add(otherPart);
                 }
+            }
+            // 保护基座防止门被简介破坏
+            if (baseBlock != null) {
+                relateBlocks.add(baseBlock);
             }
         }
     }
